748b18eda43dedff529ca05991fc5e8630ce7bc5
Saheli Jati <sahelijati130@gmail.com>
2024-02-10 20:42:23 +0530

    Update explore.py

--- explore.py
+++ explore.py
@@ -18,10 +18,44 @@ df = load_data()
 def show_explore():
     df = pd.read_csv('retail_price.csv')
     st.title("Explore the Data")
-    fig = px.scatter(df,
+    fig1 = px.scatter(df,
                      x='qty',
                      y='total_price', trendline='ols',
                      title='Quantity vs Total Price')
-    st.pyplot(fig)
+    st.plotly_chart(fig1)
+
+    fig2 = px.bar(df, x='product_category_name',
+                 y='total_price', title='Total Price by Product Category')
+    st.plotly_chart(fig2)
+
+    fig3 = px.box(df, x='weekday',
+                 y='total_price',
+                 title='Box Plot of Total Price by number of Weekdays in a Month')
+    st.plotly_chart(fig3)
+
+    df['comp1_diff'] = df['unit_price'] - df['comp_1']
+    df['comp2_diff'] = df['unit_price'] - df['comp_2']
+    df['comp3_diff'] = df['unit_price'] - df['comp_3']
+
+    for i in range(1, 4):
+        comp = f"comp{i}_diff"
+        fig4 = px.bar(x=df['product_category_name'],
+                     y=df[comp],
+                     title=f"Competitor {i} Price Difference per Unit",
+                     labels={
+                         'x': 'Product Category',
+                         'y': f'Competitor {i}'
+                     })
+        st.plotly_chart(fig4)
+    monthly_df = df.groupby(by='month_year').agg({
+        'unit_price': 'mean', 'total_price': 'sum', 'freight_price': 'sum',
+        'qty': 'sum', 'weekday': 'sum', 'weekend': 'sum', 'customers': 'sum'
+    }).reset_index()
+    monthly_df['month_year'] = pd.to_datetime(monthly_df['month_year'], format='%d-%m-%Y')
+    monthly_df = monthly_df.sort_values(by='month_year')
+    fig5 = px.scatter(monthly_df,
+                     x='customers', y='total_price', trendline='ols',
+                     title='Total Price vs Number of Customers')
+    st.plotly_chart(fig5)
 
 
